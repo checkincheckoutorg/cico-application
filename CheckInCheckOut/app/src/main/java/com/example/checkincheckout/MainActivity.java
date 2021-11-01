2ca4e609e65a818024c679e3bee08960becabc87
jonmeli <jondmeli@gmail.com>
2021-10-31 20:15:30 -0400

    Project Folder/Source + Splash Screen
    First upload of the android studio project with implementation of a splash screen.

---
+++ CheckInCheckOut/app/src/main/java/com/example/checkincheckout/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.checkincheckout;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class MainActivity extends AppCompatActivity {
+    Handler h = new Handler();
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, NextActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },5000);
+    }
+}
