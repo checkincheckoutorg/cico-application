c54c1aadaa6864c221c3a08d9e5139e8dd0025fb
jonmeli <jondmeli@gmail.com>
2021-11-03 19:22:52 -0400

    Welcome/Icon
    Got rid of the Welcome secondary splash screen, as well as updated the icons logo.

--- CheckInCheckOut/app/src/main/java/com/example/checkincheckout/MainActivity.java
+++ CheckInCheckOut/app/src/main/java/com/example/checkincheckout/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         h.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent i = new Intent(MainActivity.this, NextActivity.class);
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
                 startActivity(i);
                 finish();
             }
